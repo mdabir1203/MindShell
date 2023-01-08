f2d4e45daef8dacdaa00642b751289848f499d68
nick-ras <86893086+nick-ras@users.noreply.github.com>
2023-01-08 14:54:34 +0100

    Update clean_up.c

--- src/clean_up.c
+++ src/clean_up.c
@@ -3,6 +3,6 @@
 
 void	clean_up(int clean_up_code)
 {
-	if (clean_up_code == CTRL_D_PRESSED)
-		exit(0);
+	if (clean_up_code == CTRL_D_PRESSED) 
+		exit(0); //should we use rrl_clear_history here to free history? maybe we can check with valgrind? please correct me if i dont understand (nick)
 }
