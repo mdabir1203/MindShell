204b81e0897107cb43bcf9db8bbf2ab2c22fa052
nick-ras <86893086+nick-ras@users.noreply.github.com>
2023-01-10 10:36:12 +0100

    Update process_input.c

--- src/process_input.c
+++ src/process_input.c
@@ -6,7 +6,7 @@
  **/
 void	cmd_identifier(char **cmd)
 {
-	//we need to count number of arguments ex "cd dir1 | cat -e " is 5 argument and 2 of them is commands/executables (we have to check all arguments with access) (Nick wrote it)
+	//we need to count number of arguments ex "cd dir1 | cat -e " is 5 argument and 2 of them is commands/executables (Nick or Nadine can make function that checks if an argument is an executable) (Nick wrote it)
 	if (!strncmp(cmd[0], "echo", 5))
 		ft_echo(cmd);
 }
