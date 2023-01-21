2e6c458ebef60401a71181a1964542513ebf0840
MaxIhme <maxrehberg@posteo.de>
2023-01-21 13:58:09 +0100

    here comes the idea

--- src/parser.c
+++ src/parser.c
@@ -29,7 +29,13 @@ void	parser(t_info *info)
 	
 	while (info->input_lexer[++i])
 	{
-													// ft_order can also be split in subfunctions -> one for testing only correct redirections (< test) (>>hallo.txt) and another subfunc can test 
+													// Ranja, I got another idea. Let ft_order only test if the redirections are valid (< test) (>>hallo.txt),
+													// also valid redirection: (> -na) -> if we can't name a file like this... open() (later in the execution part) will return a -1 and we can throw an error then...
+													// -> reason for "only test if the redirections are valid": we have to test if the first word, beside all redirections, is a command, like this
+													// (<test echo >> out.txt Hallo du da) -> this works even if echo does not take the input from test it writes "Hallo du da" into the file out.txt
+													// I thought for doing it smart and simple we could first check if the redirections are correct and then cut them out of the group
+													// -> this (<test echo >> out.txt Hallo du da) will then be (echo Hallo du da) and voila, it's very easy to check now if there is a command at the first place in the group
+													// p.s. before cutting out the redirections we have to save them in the group struct -> we can thing about that later
  		printf("%s__", info->input_lexer[i]);		// only for testing purposes
 		cat = categorize(info->input_lexer[i]);
 		if (cat == WORD)
