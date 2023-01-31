306b0f8d95c8f45b0ca2994cf548dd2e05ced063
rschlott <rschlott@student.42wolfsburg.de>
2023-01-31 19:33:08 +0100

    new

--- src/tests.c
+++ src/tests.c
@@ -34,7 +34,7 @@ void	print_groups(t_group *groups, t_info *info)
 	int j;
 	while (++i < info->num_groups)
 	{
-		j = 0;
+		j = -1;
 		printf("\n\t\tGROUP %d\n", i);
 		printf("cmd: \t\t\t\t_%s_\n", groups[i].cmd);
 		printf("path: \t\t\t\t_%s_\n", groups[i].path);
@@ -42,7 +42,7 @@ void	print_groups(t_group *groups, t_info *info)
 		printf("redirect_input_filename: \t_%s_\n", groups[i].redirect_input_filename);
 		printf("redirect_output: \t\t_%d_\n", groups[i].redirect_output);
 		printf("redirect_output_filename:\t_%s_\n", groups[i].redirect_output_filename);
-		while (groups[i].arguments[j++])
+		while (groups[i].arguments[++j])
 			printf("\targument %d:\t_%s_\n", j, groups[i].arguments[j]);
 		// printf("argument_filename\t_%s_\n\n", groups[i].argument_filename);
 		printf("pipe_in: \t\t_%d_\n", groups[i].pipe_in);
