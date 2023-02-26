6997999e6c054e0fb05e4433bc6bc00e73248b06
nick-ras <nick-ras@mail.dk>
2023-02-26 18:24:33 +0100

    updated unset remove all paths

--- src/executer.c
+++ src/executer.c
@@ -169,6 +169,7 @@ void	fork_and_execve(t_group *group)
 
 void	builtin_no_piping(t_group *group)
 {
+	printf("args: %s and %s\n", group->arguments[0], group->arguments[1]);
 	if (group->builtin == CMD_EXPORT)
 		ft_export(group->arguments, group->info);
 	else if (group->builtin == CMD_UNSET)
