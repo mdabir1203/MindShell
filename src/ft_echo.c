a4d78325af56322021b1ef2650743abcf3495c6d
rschlott <rschlott@student.42wolfsburg.de>
2023-02-27 09:31:09 +0100

    echo-flag

--- src/ft_echo.c
+++ src/ft_echo.c
@@ -1,11 +1,13 @@
 
 #include "../inc/minishell.h"
 
+// -n without the \n
 void	ft_echo(char	**args, int pipe_out)
 {
 	int arg_j;
 
 	arg_j = 0;
+	printf("pipe out %d\n", pipe_out);
 	if (!strncmp(args[1], "-n", 2))
 	{
 		++arg_j;
@@ -22,36 +24,8 @@ void	ft_echo(char	**args, int pipe_out)
 		{
 			ft_putstr_fd(args[arg_j], 1);	// counting 
 			ft_putchar_fd(' ', 1);
-			//printf("arg_j %d", arg_j);
 		}
-		//if (args[arg_j])
-			//ft_putchar_fd('\n', 1);
-		if (args[arg_j] || !(pipe_out == 1))
+		if (args[arg_j] || !(pipe_out == 1))	// doesn't give an empty line for "echo" without args anymore
 			ft_putchar_fd('\n', 1);
 	}
 }
-
-// -n without the \n
-/*void	ft_echo(char **cmd)
-{
-	int i;
-	int	flag;
-
-	i = 1;
-	flag = 0;
-	while (cmd[i])
-	{
-		if (!strncmp(cmd[i], "-n", 3))
-		{
-			i++;
-			flag = 1;
-		}
-		ft_putstr_fd(cmd[i], 1);
-		if (cmd[i+1] != NULL)
-			ft_putchar_fd(' ', 1);
-		i++;
-	}
-	if (flag == 1)
-		ft_putchar_fd('%', 1);
-	ft_putchar_fd('\n', 1);
-}*/
