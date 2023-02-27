edd0a3890e133188ea0b00c88764469345eac3b3
rschlott <rschlott@student.42wolfsburg.de>
2023-02-27 09:06:45 +0100

    new

--- src/ft_echo.c
+++ src/ft_echo.c
@@ -6,16 +6,29 @@ void	ft_echo(char	**args, int pipe_out)
 	int arg_j;
 
 	arg_j = 0;
-	while (args[++arg_j])	// starting from 1 as 0 is the command itself
+	if (!strncmp(args[1], "-n", 2))
 	{
-		ft_putstr_fd(args[arg_j], 1);	// counting 
-		ft_putchar_fd(' ', 1);
-		//printf("arg_j %d", arg_j);
+		++arg_j;
+		while (args[++arg_j])	// starting from 1 as 0 is the command itself
+		{
+			ft_putstr_fd(args[arg_j], 1);
+			ft_putchar_fd(' ', 1);
+		}
+		ft_putstr_fd("\b", 1);
 	}
-	//if (args[arg_j])
-		//ft_putchar_fd('\n', 1);
-	if (args[arg_j] || !(pipe_out == 1))
-		ft_putchar_fd('\n', 1);
+	else
+	{
+		while (args[++arg_j])	// starting from 1 as 0 is the command itself
+		{
+			ft_putstr_fd(args[arg_j], 1);	// counting 
+			ft_putchar_fd(' ', 1);
+			//printf("arg_j %d", arg_j);
+		}
+		//if (args[arg_j])
+			//ft_putchar_fd('\n', 1);
+		if (args[arg_j] || !(pipe_out == 1))
+			ft_putchar_fd('\n', 1);
+	}
 }
 
 // -n without the \n
