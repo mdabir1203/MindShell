89507893abda833c3cb8b0f596d104075ddc438c
MaxIhme <maxrehberg@posteo.de>
2023-03-01 09:19:54 +0100

    some stuff

--- src/ft_echo.c
+++ src/ft_echo.c
@@ -8,7 +8,7 @@ void	ft_echo(char	**args, int pipe_out)
 
 	arg_j = 0;
 	//printf("pipe out %d\n", pipe_out);
-	if (!strncmp(args[1], "-n", 2))
+	if (!ft_strncmp(args[1], "-n", 2))
 	{
 		++arg_j;
 		while (args[++arg_j])	// starting from 1 as 0 is the command itself
@@ -25,7 +25,7 @@ void	ft_echo(char	**args, int pipe_out)
 			ft_putstr_fd(args[arg_j], 1);	// counting 
 			ft_putchar_fd(' ', 1);
 		}
-		if (args[arg_j] || !(pipe_out == 1))	// doesn't give an empty line for "echo" without args anymore
+		if (!(pipe_out == 1) || args[arg_j])	// doesn't give an empty line for "echo" without args anymore
 			ft_putchar_fd('\n', 1);
 	}
 }
