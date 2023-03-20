9c7456c827d32e5719f5ee8ea4f7244d39341f9b
MaxIhme <maxrehberg@posteo.de>
2023-03-20 21:07:24 +0100

    norminetted heredoc

--- src/heredoc.c
+++ src/heredoc.c
@@ -1,13 +1,22 @@
 #include "../inc/minishell.h"
 
-int heredoc_sub(t_group *group, char *str[3], int fd[2]) //temp str[0] , buf str[1]
+/**
+ * @brief //temp str[0] , buf str[1]
+ * 
+ * @param group 
+ * @param str 
+ * @param fd 
+ * @return int 
+ */
+int	heredoc_sub(t_group *group, char *str[3], int fd[2])
 {
-	while (!str[0][0] || ft_strncmp(str[0], group->redir_infile, ft_strlen(group->redir_infile) + 1))
+	while (!str[0][0] || ft_strncmp(str[0], group->redir_infile, \
+		ft_strlen(group->redir_infile) + 1))
 	{
 		free (str[0]);
-		str[0] = readline("> "); // ctrtl D händeln  also return wert von 0
+		str[0] = readline("> ");
 		if (!str[0])
-			break;
+			break ;
 		str[2] = str[1];
 		str[1] = ft_strjoin(str[1], str[0]);
 		free(str[2]);
@@ -18,28 +27,10 @@ int heredoc_sub(t_group *group, char *str[3], int fd[2]) //temp str[0] , buf str
 	if (!str[0])
 		write(fd[1], str[1], ft_strlen(str[1]));
 	else
-		write(fd[1], str[1], ft_strlen(str[1]) - ft_strlen(group->redir_infile) - 1);
+		write(fd[1], str[1], ft_strlen(str[1]) - \
+			ft_strlen(group->redir_infile) - 1);
 	close(fd[1]);
 	return (fd[0]);
-
-	// while (!temp[0] || ft_strncmp(temp, group->redir_infile, ft_strlen(group->redir_infile) + 1))
-	// {
-	// 	free (temp);
-	// 	temp = readline("> "); // ctrtl D händeln  also return wert von 0
-	// 	if (!temp)
-	// 		break;
-	// 	buf_ = buf;
-	// 	buf = ft_strjoin(buf, temp);
-	// 	free(buf_);
-	// 	buf_ = buf;
-	// 	buf = ft_strjoin(buf, "\n");
-	// 	free (buf_);
-	// }
-	// if (!temp)
-	// 	write(fd[1], buf, ft_strlen(buf));
-	// else
-	// 	write(fd[1], buf, ft_strlen(buf) - ft_strlen(group->redir_infile) - 1);
-	// close(fd[1]);
 }
 
 /**
@@ -48,11 +39,11 @@ int heredoc_sub(t_group *group, char *str[3], int fd[2]) //temp str[0] , buf str
  * 
  * @return int 
  */
-int	heredoc(t_group *group) // check fd's closed // save mallocs ? 
+int	heredoc(t_group *group)
 {
-	int fd[2];
+	int		fd[2];
 	char	*str[3];
-	
+
 	str[0] = malloc(1);
 	str[0][0] = 0;
 	str[1] = malloc(1);
@@ -67,7 +58,7 @@ int	heredoc(t_group *group) // check fd's closed // save mallocs ?
 	}
 	if (pipe(fd) == -1)
 	{
-		perror("pipe fail"); // do we need to save pipes?
+		perror("pipe fail");
 		return (-1);
 	}
 	fd[0] = heredoc_sub(group, str, fd);
