d442ef6e5db28c6030aa1fffc40163c18f3e27e6
MaxIhme <maxrehberg@posteo.de>
2023-03-21 17:35:28 +0100

    norminetted error.c ft_env.c ft_exit.c ft_message.c expand_variables.c tests.c redirect_and_pipe_helpers.c

--- src/ft_env.c
+++ src/ft_env.c
@@ -1,20 +1,12 @@
 #include "../inc/minishell.h"
 
-void	ft_env(t_info *info)
+void	ft_env_sub(t_info *info)
 {
 	int	i;
 	int	j;
 	int	k;
-	int argc;
 
 	i = -1;
-	argc = 0;
-	while (info->groups->arguments[argc])
-		argc++;
-	if (argc > 1) {
-		printf("env: %s No such file or directory\n", info->groups->arguments[1]);
-		return;
-	}
 	while (info->env[++i])
 	{
 		j = -1;
@@ -24,8 +16,38 @@ void	ft_env(t_info *info)
 			while (info->env[i][j][++k])
 				write(1, &info->env[i][j][k], 1);
 			if (j == 0)
-				write(1, "=", 1); //print = after first char
+				write(1, "=", 1);
 		}
 		printf("\n");
 	}
 }
+
+void	ft_env(t_info *info)
+{
+	int	argc;
+
+	argc = 0;
+	while (info->groups->arguments[argc])
+		argc++;
+	if (argc > 1)
+	{
+		printf("env: %s No such file or directory\n", \
+			info->groups->arguments[1]);
+		return ;
+	}
+	ft_env_sub(info);
+}
+
+	// while (info->env[++i])
+	// {
+	// 	j = -1;
+	// 	while (info->env[i][++j])
+	// 	{
+	// 		k = -1;
+	// 		while (info->env[i][j][++k])
+	// 			write(1, &info->env[i][j][k], 1);
+	// 		if (j == 0)
+	// 			write(1, "=", 1);
+	// 	}
+	// 	printf("\n");
+	// }
