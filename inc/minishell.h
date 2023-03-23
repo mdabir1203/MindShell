6491d8ed67710f12e793a388897385489c23eeb5
rschlott <rschlott@student.42wolfsburg.de>
2023-03-23 16:58:54 +0100

    new

--- inc/minishell.h
+++ inc/minishell.h
@@ -2,20 +2,19 @@
 # define MINISHELL_H
 
 # include "../libs/libft/libft.h"
-# include <sys/stat.h>
-# include <sys/stat.h>
-# include <sys/ioctl.h>
-# include <stdlib.h>
-# include <stdio.h>
-# include <unistd.h>
-# include <string.h>
-# include <signal.h>
-# include <fcntl.h>
 # include <dirent.h>
-# include <readline/readline.h>
+# include <fcntl.h>
 # include <readline/history.h>
+# include <readline/readline.h>
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/ioctl.h>
+# include <sys/stat.h>
 # include <sys/wait.h>
 # include <termios.h>
+# include <unistd.h>
 
 //*********************************************************//
 //**                FLAGS                               **//
@@ -121,7 +120,7 @@ typedef struct s_info
 	int				nb_root_cmd;
 	int				num_groups;
 	struct s_group	*groups;
-}	t_info;
+}					t_info;
 
 /*
 The code is designed to parse commands typed into the minishell,
@@ -137,25 +136,25 @@ The parsed information is then used in the executer.
 */
 typedef struct s_group
 {
-	int		exit_status;
-	char	**arguments;
-	char	*path;
-	int		builtin;
-	int		redir_in;
-	char	*redir_infile;
-	int		redir_out;
-	char	*redir_outfile;
-	int		pipe_in;
-	int		pipe_out;
-	int		pipe_fd[2];
-	pid_t	pid;
-	t_info	*info;
-}	t_group;
+	int				exit_status;
+	char			**arguments;
+	char			*path;
+	int				builtin;
+	int				redir_in;
+	char			*redir_infile;
+	int				redir_out;
+	char			*redir_outfile;
+	int				pipe_in;
+	int				pipe_out;
+	int				pipe_fd[2];
+	pid_t			pid;
+	t_info			*info;
+}					t_group;
 
 /**
  * @brief temp struct for the while loop in the parser
  * 
- * @cat: 	    the int value the categorizer returns f.e. (REDIRECT_OUTPUT)
+ * @cat: 		 the int value the categorizer returns f.e. (REDIRECT_OUTPUT)
  * @act_group:	actual group of commands f.e. (echo Hallo | cat) before the 
  *              | it's 0 after it's 1
  * @is_red:		is redirect -> the int value that found_save_redirect()
@@ -165,13 +164,12 @@ typedef struct s_group
  */
 typedef struct s_parse_lexer
 {
-	int		cat;
-	int		act_group;
-	int		is_red;
-	int		is_exe;
+	int				cat;
+	int				act_group;
+	int				is_red;
+	int				is_exe;
 
-}	t_parse_lexer;
-
+}					t_parse_lexer;
 
 //*********************************************************//
 //**                FUNCTIONS                           **//
@@ -179,209 +177,213 @@ typedef struct s_parse_lexer
 
 //**** categorize.c ****//
 
-int		categorize(char *str);
+int					categorize(char *str);
 
 //**** check_if_cmd.c ****//
-void	free_slash(char *slash_added, char *slash_not_added);
-int	check_if_access_in_parser(char *cmd, t_info *t_info, \
-char	**path_to_executable);
+void				free_slash(char *slash_added, char *slash_not_added);
+int					check_if_access_in_parser(char *cmd, t_info *t_info,
+						char **path_to_executable);
 
-char	*is_an_executable(char *cmd, t_info *t_info);
+char				*is_an_executable(char *cmd, t_info *t_info);
 
 //**** check_if_cmd_utils.c****//
-char *check_if_path_found(char *path_to_executable, t_info *t_info, \
-char *slash_added, char *slash_not_added);
+char				*check_if_path_found(char *path_to_executable,
+						t_info *t_info, char *slash_added,
+						char *slash_not_added);
 
 //**** input_check.c ****//
 
-int		parser_error_check(t_info *info, t_parse_lexer *pl, int i);
-int		executer_error_check(t_info *info, t_group *groups);
+int					parser_error_check(t_info *info, t_parse_lexer *pl, int i);
+int					executer_error_check(t_info *info, t_group *groups);
 
 //**** clean_up_utils.c ****//
 
-void	clean_up_group_structs(t_info *info);
-void	clean_up_info_struct(t_info *info);
-void	clean_up_paths(t_info *info);
-void	clean_up_prompt(t_info *info);
-void	clean_up_env(t_info *info);
+void				clean_up_group_structs(t_info *info);
+void				clean_up_info_struct(t_info *info);
+void				clean_up_paths(t_info *info);
+void				clean_up_prompt(t_info *info);
+void				clean_up_env(t_info *info);
 
 //**** clean_up.c ****//
 
-void	clean_up_lexer(t_info *info);
-void	clean_up(int clean_up_code, t_info *info);
+void				clean_up_lexer(t_info *info);
+void				clean_up(int clean_up_code, t_info *info);
 
 //**** ft_message.c ****//
 
-int		input_message(char *str, int exit_no);
+int					input_message(char *str, int exit_no);
 
 //**** error.c ****//
 
-int		error(int err, t_info *info);
+int					error(int err, t_info *info);
 
 //**** executer.c ****//
 
-void	executer(t_group	*group);
+void				executer(t_group *group);
 
 //**** expand_variables_utils.c ****//
 
-int		var_delimiter(char c);
-int		variable_name_count(char *str);
-char	*exit_status(void);
-char	*value_of_variable_from_env(char ***env, char *var, int len);
-char	*replace_first_var_in_str(char *str, char *value, \
-	int num_var, int num_value);
+int					var_delimiter(char c);
+int					variable_name_count(char *str);
+char				*exit_status(void);
+char				*value_of_variable_from_env(char ***env, char *var,
+						int len);
+char				*replace_first_var_in_str(char *str, char *value,
+						int num_var, int num_value);
 
 //**** expand_variables.c ****//
 
-char	*value_of_variable_from_env(char ***env, char *var, int len);
-int		expand_variables(char	**array, t_info *info);
+char				*value_of_variable_from_env(char ***env, char *var,
+						int len);
+int					expand_variables(char **array, t_info *info);
 
 //**** ft_pwd.c ***//
 
-void	ft_pwd(t_group *group);
+void				ft_pwd(t_group *group);
 
 //**** ft_cd.c ****//
 
-void	ft_cd(char **args, t_info *info);
+void				ft_cd(char **args, t_info *info);
 
 //**** ft_echo.c ****//
 
-void	ft_echo(char	**args, int pipe_out);
+void				ft_echo(char **args, int pipe_out);
 
 //**** ft_exit.c ****//
 
-void	ft_exit(t_info *info);
+void				ft_exit(t_info *info);
 
 //**** ft_env.c ****//
-void	ft_env(t_info *info);
+void				ft_env(t_info *info);
 
 //**** ft_clear.c ****//
-void	ft_clear(void);
+void				ft_clear(void);
 
 //**** ft_export_sub_sub.c ****//
 
-void	populate_var(char *str, char *arg);
-void	populate_cont(char *str, char *arg);
-void	export_with_no_args(t_info *info);
-int		var_pos_in_env_export(char *arg, char ***env);
-void	replace_cont_of_var(char *arg, char ***env, t_info *info);
+void				populate_var(char *str, char *arg);
+void				populate_cont(char *str, char *arg);
+void				export_with_no_args(t_info *info);
+int					var_pos_in_env_export(char *arg, char ***env);
+void				replace_cont_of_var(char *arg, char ***env, t_info *info);
 
 //**** ft_export_sub.c ****//
 
-int		num_args(char **args);
-int		num_env_args(char ***args);
-void	populate_new_env_with_old_args(char ***new_env, char ***old_env);
-int		num_var_chars(char *str);
-int		num_cont_chars(char *str);
+int					num_args(char **args);
+int					num_env_args(char ***args);
+void				populate_new_env_with_old_args(char ***new_env,
+						char ***old_env);
+int					num_var_chars(char *str);
+int					num_cont_chars(char *str);
 
 //**** ft_export.c ****//
 
-int		num_env_args(char ***args);
-int		ft_export(char **args, t_info *info);
+int					num_env_args(char ***args);
+int					ft_export(char **args, t_info *info);
 
 //**** ft_split_lexer_utils_2.c ****//
 
-char	*after_sep(char *str, int *buf);
-char	*after_pipe(char *str, int *buf);
-char	*after_quote(char *str, int *buf);
-char	*after_word(char *str, int *buf);
-int		count_parts(char *str);
+char				*after_sep(char *str, int *buf);
+char				*after_pipe(char *str, int *buf);
+char				*after_quote(char *str, int *buf);
+char				*after_word(char *str, int *buf);
+int					count_parts(char *str);
 
 //**** ft_split_lexer_utils.c ****//
 
-int		sep(char c);
-int		red(char c);
-int		quote(char c);
-int		pipesign(char c);
-char	*after_red(char *str, int *buf);
+int					sep(char c);
+int					red(char c);
+int					quote(char c);
+int					pipesign(char c);
+char				*after_red(char *str, int *buf);
 
 //**** ft_split_lexer.c ****//
 
-int		sep(char c);
-int		count_parts(char *str);
-char	**ft_split_lexer(char *str, t_info *info);
+int					sep(char c);
+int					count_parts(char *str);
+char				**ft_split_lexer(char *str, t_info *info);
 
 //**** ft_unset.c ****//
 
-int		ft_unset(char **args, t_info *info);
+int					ft_unset(char **args, t_info *info);
 
 //**** init.c ****//
 
-t_info	*init(char **envp);
-char	**ft_array_args(char *str, t_info *info);
-t_group	*init_groups(t_info *info);
-
+t_info				*init(char **envp);
+char				**ft_array_args(char *str, t_info *info);
+t_group				*init_groups(t_info *info);
 
 //**** make_env_arr.c ****//
 
-void	make_env(char **envp, t_info *info);
+void				make_env(char **envp, t_info *info);
 
 //**** parser_utils_3.c ****//
 
-int		count_groups(t_info *info);
-void	shift_str_left(char *str);
-void	delete_quotationmarks(char	**array);
+int					count_groups(t_info *info);
+void				shift_str_left(char *str);
+void				delete_quotationmarks(char **array);
 
 //**** parser_utils_2.c ****//
 
-int		is_redirect(int num);
-int		found_save_redirect_sub(int *before_cat, t_info *info, \
-	char *act_lexer, t_parse_lexer *pl);
-int		found_save_redirect(t_parse_lexer *pl, t_info *info, \
-	char *act_input_lexer_str, int i);
-int		is_builtin(char *str);
+int					is_redirect(int num);
+int					found_save_redirect_sub(int *before_cat, t_info *info,
+						char *act_lexer, t_parse_lexer *pl);
+int					found_save_redirect(t_parse_lexer *pl, t_info *info,
+						char *act_input_lexer_str, int i);
+int					is_builtin(char *str);
 
 //**** parser_utils.c ****//
 
-int		found_save_redirect(t_parse_lexer *pl, t_info *info, \
-char *act_input_lexer_str, int i);
-int		found_save_executable(t_parse_lexer *pl, t_info *info, \
-char *act_input_lexer_str, int i);
-void	found_save_arguments(t_parse_lexer *pl, t_info *info, int i);
-void	pipe_detector(t_parse_lexer *pl, t_info *info);
-int		count_groups(t_info *info);
-void	delete_quotationmarks(char	**array);
+int					found_save_redirect(t_parse_lexer *pl, t_info *info,
+						char *act_input_lexer_str, int i);
+int					found_save_executable(t_parse_lexer *pl, t_info *info,
+						char *act_input_lexer_str, int i);
+void				found_save_arguments(t_parse_lexer *pl, t_info *info,
+						int i);
+void				pipe_detector(t_parse_lexer *pl, t_info *info);
+int					count_groups(t_info *info);
+void				delete_quotationmarks(char **array);
 
 //**** parser.c ****//
 
-int		parser(t_info *info);
+int					parser(t_info *info);
 
 //**** redirect_and_pipe_helpers.c ****//
 
-int		redir_in(t_group *group);
-void	redir_out(t_group *group);
-void	make_pipe(t_group *group);
-void	replace_pipe_in_next_group(t_group *group, int new_pipe_in);
-int		next_have_pipe_out(t_group *group);
+int					redir_in(t_group *group);
+void				redir_out(t_group *group);
+void				make_pipe(t_group *group);
+void				replace_pipe_in_next_group(t_group *group, int new_pipe_in);
+int					next_have_pipe_out(t_group *group);
 
 //**** helpers_fds_builtin.c ****//
 
-void	dup_fd(int fd_new, int fd_old, t_info *info);
-void	closing_fds(t_group *group);
-int		check_access_infile_outfile(t_group *group);
-void	builtins_with_output(t_group *group);
-void	builtin_no_piping(t_group *group);
+void				dup_fd(int fd_new, int fd_old, t_info *info);
+void				closing_fds(t_group *group);
+int					check_access_infile_outfile(t_group *group);
+void				builtins_with_output(t_group *group);
+void				builtin_no_piping(t_group *group);
 
 //**** heredoc.c ****//
-int		heredoc_sub(t_group *group, char *str[3], int fd[2]);
-int		heredoc(t_group *group);
+int					heredoc_sub(t_group *group, char *str[3], int fd[2]);
+int					heredoc(t_group *group);
 
 //**** helpers_fds.c ****//
-int		check_access_infile_outfile(t_group *group);
-void	dup_fd(int fd_new, int fd_old, t_info *info);
-void	closing_fds(t_group *group);
-void	handle_redir_and_dup(t_group *group);
+int					check_access_infile_outfile(t_group *group);
+void				dup_fd(int fd_new, int fd_old, t_info *info);
+void				closing_fds(t_group *group);
+void				handle_redir_and_dup(t_group *group);
 
 //**** ft_wait_and_exit.c ****//
-void	wait_and_exit_status(t_group *tmp);
+void				wait_and_exit_status(t_group *tmp);
 
 //**** tests.c ****//
 
-void	p2d(char **ptr);
-void	print_groups(t_group *groups, t_info *info);
+void				p2d(char **ptr);
+void				print_groups(t_group *groups, t_info *info);
 
-void	remove_ctrl_c_feed(void);
+void				remove_ctrl_c_feed(void);
 
-extern int	g_exit_status;
+extern int			g_exit_status;
 
 #endif
--- src/ft_echo.c
+++ src/ft_echo.c
@@ -1,36 +1,40 @@
-
 #include "../inc/minishell.h"
 
-// -n without the \n
-void	ft_echo(char	**args, int pipe_out)
+/*
+* shortens ft_echo function < 25 lines.
+*/
+void	ft_echo_helper(char **args, int arg_j)
 {
-	int arg_j;
+	while (args[++arg_j])
+	{
+		ft_putstr_fd(args[arg_j], 1);
+		ft_putchar_fd(' ', 1);
+	}
+}
+
+/*
+* -n without the \n
+*/
+void	ft_echo(char **args, int pipe_out)
+{
+	int	arg_j;
 
 	arg_j = 0;
-	//printf("pipe out %d\n", pipe_out);
 	if (!args[1])
 	{
-		if (!(pipe_out == 1) || args[1])	// doesn't give an empty line for "echo" without args anymore
+		if (!(pipe_out == 1) || args[1])
 			ft_putchar_fd('\n', 1);
 	}
 	else if (!ft_strncmp(args[1], "-n", 2))
 	{
 		++arg_j;
-		while (args[++arg_j])	// starting from 1 as 0 is the command itself
-		{
-			ft_putstr_fd(args[arg_j], 1);
-			ft_putchar_fd(' ', 1);
-		}
+		ft_echo_helper(args, arg_j);
 		ft_putstr_fd("\b", 1);
 	}
 	else
 	{
-		while (args[++arg_j])	// starting from 1 as 0 is the command itself
-		{
-			ft_putstr_fd(args[arg_j], 1);	// counting 
-			ft_putchar_fd(' ', 1);
-		}
-		if (!(pipe_out == 1) || args[arg_j])	// doesn't give an empty line for "echo" without args anymore
+		ft_echo_helper(args, arg_j);
+		if (!(pipe_out == 1) || args[arg_j])
 			ft_putchar_fd('\n', 1);
 	}
- }
+}
